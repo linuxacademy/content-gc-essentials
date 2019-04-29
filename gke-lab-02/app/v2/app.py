83cfb79571d5a6d19d8a8d29536f25de37308ded
Joe-at-LA <36517206+Joe-at-LA@users.noreply.github.com>
2019-04-29 11:21:02 -0400

    Add files via upload

---
+++ gke-lab-02/app/v2/app.py
@@ -0,0 +1,14 @@
+# The Docker image contains the following code
+from flask import Flask
+import os
+import socket
+
+app = Flask(__name__)
+
+@app.route("/")
+def showPinehead():
+    html = "<div style='text-align:center;margin:20px;'><h1>Greetings from Linux Academy!</h1><h2>Version 2</h2><img src='https://storage.googleapis.com/la-gcp-labs-resources/essentials/Logo-Pinehead-NVY.png' width='40%' alt='Pinehead @ Linux Academy'></div>"
+    return html
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', port=80)
