a1e1fe088f70d81c9f544adf93ee5d8ee273ef86
surajp24 <surajp24@users.noreply.github.com>
2019-10-30 21:00:37 +0530

    Update app.py

--- gke-lab-01/app.py
+++ gke-lab-01/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def showPinehead():
-    html = "<div style='text-align:center;margin:20px;'><h1>Greetings from Linux Academy!</h1><img src='https://storage.googleapis.com/la-gcp-labs-resources/essentials/Logo-Pinehead-NVY.png' width='40%' alt='Pinehead @ Linux Academy'></div>"
+    html = "<div style='text-align:center;margin:20px;'><h1>Greetings from Suraj Academy!</h1><img src='https://storage.googleapis.com/la-gcp-labs-resources/essentials/Logo-Pinehead-NVY.png' width='40%' alt='Pinehead @ Linux Academy'></div>"
     return html
 
 if __name__ == "__main__":
